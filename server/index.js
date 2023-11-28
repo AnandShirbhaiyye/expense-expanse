116ac2cbaa28f3126fd280e0cf697f0ae4247fe8
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-11-28 14:36:29 +0530

    create get api

--- server/index.js
+++ server/index.js
@@ -8,47 +8,57 @@ const app = express();
 app.use(express.json());
 
 async function connectMongoDB() {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    if (conn) {
-      console.log("Connected to MongoDB📦");
-    }
+  const conn = await mongoose.connect(process.env.MONGODB_URI);
+  if (conn) {
+    console.log("Connected to MongoDB📦");
   }
+}
 connectMongoDB();
 
-app.get('/health', async (req, res)=>{
+app.get("/health", async (req, res) => {
   res.json({
     success: true,
-    message:" All Good✅"
-  })
-})
+    message: " All Good✅",
+  });
+});
 
-app.post('/api/transaction', async (req, res)=>{
-  const {amount, type, category, description} = req.body;
+app.post("/api/transaction", async (req, res) => {
+  const { amount, type, category, description } = req.body;
 
-  const transaction = new Transaction ({
+  const transaction = new Transaction({
     amount,
     category,
     type,
-    description
+    description,
   });
-  try{
+  try {
     const savedTransaction = await transaction.save();
 
     return res.json({
       success: true,
-      message: 'Transaction saved successfully',
-      transaction: savedTransaction
-    })
-  }catch(err){
-   return res.json({
-    success: false,
-    message: err.message
-   })
+      message: "Transaction saved successfully",
+      data: savedTransaction,
+    });
+  } catch (err) {
+    return res.json({
+      success: false,
+      message: err.message,
+    });
   }
+});
+
+app.get("/api/transactions", async (req, res) => {
+  const allTransactions = await Transaction.find();
+
+  return res.json({
+    success: true,
+    message: "successfully fetched all transaction",
+    data: allTransactions,
+  });
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-  console.log(`Server is running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
