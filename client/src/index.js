3c051037f5da244bbc08317759bed81bd5819458
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-12-05 00:11:33 +0530

    feat: added routes and create some changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+
+//boostrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Home from './views/Home/Home';
+import Login from './views/Login/Login';
+import SignUp from './views/SignUp/SignUp';
+import AddTransactions from './views/AddTransactions'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+      path:'/',
+      element:<Home/>
+  },
+  {
+      path:'/login',
+      element:<Login/>
+  },
+  {
+      path:'/signup',
+      element:<SignUp/>
+  },
+  {
+    path:'/addtransactions',
+    element:<AddTransactions/>
+  },
+])
+root.render(<RouterProvider router = {router} />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
