c8059861b6d394edefea0dfb4979c835a4c11a69
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-12-04 23:52:24 +0530

    feat: added proxy setup

--- server/index.js
+++ server/index.js
@@ -3,16 +3,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-import  { getApiHealth } from "./controllers/health.js";
+import { getApiHealth } from "./controllers/health.js";
 import { postApiTransaction, getApiTransaction } from "./controllers/transaction.js";
 
 const app = express();
 app.use(express.json());
 
 async function connectMongoDB() {
-  const conn = await mongoose.connect(process.env.MONGODB_URI);
-  if (conn) {
-    console.log("Connected to MongoDB📦");
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    if (conn) {
+      console.log("Connected to MongoDB📦");
+    }
+  } catch (e) {
+    console.log(e.message);
   }
 }
 connectMongoDB();
