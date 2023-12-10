418e954435b60a496aa02a6ab48accc008dccbd3
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-12-10 18:53:32 +0530

    create navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link
+            className="navbar-brand  fs-4 color-title bg-body-create"
+            to="/"
+          >
+            Expense-Expanse
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse fs-5 "
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-auto ">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active me-5"
+                  aria-current="page"
+                  to="/"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active  me-5"
+                  aria-current="page"
+                  to="/addtransactions"
+                >
+                  Add Transaction
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active  me-5"
+                  aria-current="page"
+                  to="/login"
+                >
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active  me-5"
+                  aria-current="page"
+                  to="/signup"
+                >
+                  SignUp
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
