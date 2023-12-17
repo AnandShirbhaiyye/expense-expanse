bdbe2572d80879edd1b214321d0ce50971d2733b
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-12-18 00:02:30 +0530

    added some css changes in mytransaction page

--- client/src/views/MyTransactions/MyTransactions.js
+++ client/src/views/MyTransactions/MyTransactions.js
@@ -1,95 +1,108 @@
-import React, {useState, useEffect} from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import axios from 'axios';
-import "./MyTransactions.css"
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import axios from "axios";
+import "./MyTransactions.css";
 
 function MyTransactions() {
-    const [transaction, setTransaction] = useState([])
-    const [creditSum, setCreditSum] = useState(0);
-    const [debitSum, setDebitSum] = useState(0);
+  const [transaction, setTransaction] = useState([]);
+  const [creditSum, setCreditSum] = useState(0);
+  const [debitSum, setDebitSum] = useState(0);
 
-    const CATEGORY_EMOJI_MAP = {
-        "food": "🍔",
-        "entertainement": "📺",
-        "shopping": "🛍",
-        "rent": "🏡",
-        "travel": "✈",
-        "education": "🏫",
-        "salary": "💰",
-        "freelancing": "💻",
-        "side-hussle": "👔",
-        "other": "🤔"
+  const CATEGORY_EMOJI_MAP = {
+    food: "🍔",
+    entertainement: "📺",
+    shopping: "🛍",
+    rent: "🏡",
+    travel: "✈",
+    education: "🏫",
+    salary: "💰",
+    freelancing: "💻",
+    "side-hussle": "👔",
+    other: "🤔",
+  };
+
+  const loadTransaction = async () => {
+    const getUser = JSON.parse(localStorage.getItem("user") || "{}");
+    const storageUser = getUser._id;
+    console.log(storageUser);
+
+    const response = await axios.get(`/api/transactions/users/${storageUser}`);
+    const transactionsData = response?.data?.data;
+
+    let totalCredit = 0;
+    let totalDebit = 0;
+
+    transactionsData?.forEach((transaction) => {
+      if (transaction.type === "credit") {
+        totalCredit += transaction.amount;
+      } else {
+        totalDebit += transaction.amount;
       }
-   
-      const loadTransaction = async () => {
-        const getUser = JSON.parse(localStorage.getItem('user') || '{}');
-        const storageUser = getUser._id;
-        console.log(storageUser)
-    
-        const response = await axios.get(`/api/transactions/users/${storageUser}`);
-        const transactionsData = response?.data?.data;
-    
-        let totalCredit = 0;
-        let totalDebit = 0;
-    
-        transactionsData?.forEach((transaction) => {
-          if (transaction.type === "credit") {
-            totalCredit += transaction.amount;
-          } else {
-            totalDebit += transaction.amount;
-          }
-        })
-        setCreditSum(totalCredit);
-        setDebitSum(totalDebit);
-    
-        setTransaction(transactionsData);
-    
-      }
-    
-      useEffect(() => {
-        loadTransaction();
-      }, [])
+    });
+    setCreditSum(totalCredit);
+    setDebitSum(totalDebit);
+
+    setTransaction(transactionsData);
+  };
+
+  useEffect(() => {
+    loadTransaction();
+  }, []);
 
   return (
-   <>
-   <Navbar/>
-   <div className='container'>
-        <h1 className='text-center'>All Expenses</h1>
-        <h4 className='credit-text'>Credit: {creditSum}</h4>
-        <h4 >Debit: {debitSum}</h4>
-        {
-          transaction?.map((transactions, index) => {
-            const { _id, amount, type, description, category, createdAt } = transactions;
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-center">All Expenses</h1>
+        <div className="row">
+          <div className="d-flex justify-content-between">
+            <div className="card shadow-md p-2 border border-2 rounded-3 border-success">
+              <h4 className="credit-text">Credit: {creditSum}</h4>
+            </div>
+            <div className="card shadow-lg p-2 border border-2 rounded-3 border-danger">
+              <h4>Debit: {debitSum}</h4>
+            </div>
+          </div>
+        </div>
 
-            const date = new Date(createdAt).toLocaleDateString();
-            const time = new Date(createdAt).toLocaleTimeString();
-            return (
-              <div key={index} className='transaction-card'>
-                <span className={`transaction-amount ${type === 'debit' ? "debit-amount" : "credit-amount"}`}>
-                  {type === 'debit' ? "-" : "+"}
-                  {amount}</span>
-               <b>{type === 'debit' ? 'debited' : 'credited'}</b> 
+        {transaction?.map((transactions, index) => {
+          const { _id, amount, type, description, category, createdAt } =
+            transactions;
 
-                <span className='transaction-category'>
-                  {CATEGORY_EMOJI_MAP[category]}
-                  {category} 
+          const date = new Date(createdAt).toLocaleDateString();
+          const time = new Date(createdAt).toLocaleTimeString();
+          return (
+            <div key={index} className="transaction-card">
+              <span
+                className={`transaction-amount ${
+                  type === "debit" ? "debit-amount" : "credit-amount"
+                }`}
+              >
+                {type === "debit" ? "-" : "+"}
+                {amount}
+              </span>
+              <b>{type === "debit" ? "debited" : "credited"}</b>
 
-                 
-                 <button className='btn btn-success edit'>update</button>
+              <span className="transaction-category">
+                {CATEGORY_EMOJI_MAP[category]}
+                {category}
 
-                </span>
-                <hr />
-                {description}
-                <span className='date-text'> On {date} at {time}</span>
+                <button className="btn btn-success edit">update</button>
+              </span>
+              <hr />
+              {description}
+              <span className="date-text">
+                {" "}
+                On {date} at {time}
+              </span>
 
-                <p className='btn btn-danger delete'>delete</p>
-              </div>
-            )
-          })
-        }
+              <p className="btn btn-danger delete">delete</p>
+            </div>
+          );
+        })}
       </div>
-   </>
-  )
+    </>
+  );
 }
 
-export default MyTransactions
+export default MyTransactions;
